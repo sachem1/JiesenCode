2a1b971fc8b08e221f596c1092a7fb63d8e6193e
sachem1 <jieqqq678@163.com>
2019-08-16 06:52:00 +0800

    .....

---
+++ Tools/WindowsForms/Form.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsForms
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.rtbMsg = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 30);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // rtbMsg
+            // 
+            this.rtbMsg.Location = new System.Drawing.Point(0, 271);
+            this.rtbMsg.Name = "rtbMsg";
+            this.rtbMsg.Size = new System.Drawing.Size(800, 179);
+            this.rtbMsg.TabIndex = 1;
+            this.rtbMsg.Text = "";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbMsg);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox rtbMsg;
+    }
+}
+
---
+++ Tools/WindowsForms/Form.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+        private delegate void InvokeCallback(string msg);
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 11000; i++)
+            {
+                Thread.Sleep(2000);
+                WriteLog(i.ToString());
+            }
+        }
+
+        private void WriteLog(string msg)
+        {
+            Task.Factory.StartNew(() => { m_comm_MessageEvent(msg + "\r\n"); });
+            
+        }
+
+        private void APMDoing(IAsyncResult result)
+        {
+            var delegateWrite=(Func<int,string>)result.AsyncState;
+
+            var finalResult = delegateWrite.EndInvoke(result);
+            rtbMsg.AppendText(finalResult);
+        }
+
+        void m_comm_MessageEvent(string msg)
+        {
+
+            if (rtbMsg.InvokeRequired)
+            {
+
+                InvokeCallback msgCallback = new InvokeCallback(m_comm_MessageEvent);
+
+                rtbMsg.Invoke(msgCallback, new object[] { msg });
+            }
+
+            else
+            {
+                rtbMsg.AppendText(msg);
+            }
+
+        }
+    }
+}
