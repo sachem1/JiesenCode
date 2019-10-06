bef48d68864a8aed268b169f3823509d6b4258c6
jiesen <jieqqq678@163.com>
2019-10-06 22:29:48 +0800

    1

---
+++ Jiesen.WebApi/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Epass.Vue.WebApi.Models;
+using Epass.Vue.WebApi.Models.Dto;
+
+namespace Epass.Vue.WebApi.Controllers
+{
+    public class UserController : BaseController<UserModelDto>
+    {
+        private static readonly List<UserModelDto> UserList = new List<UserModelDto>();
+
+        [Route("api/user/create")]
+        [HttpPost]
+        public ReturnResult<UserModelDto> Create(UserModelDto t)
+        {
+            if (t.Name == null) return new ReturnResult<UserModelDto>() { Status = 1, Message = "用户名不能为空!" };
+            UserList.Add(t);
+            return GenerateSuccessResult(t);
+        }
+
+
+        [Route("api/user/update")]
+        [HttpPost]
+        public ReturnResult<UserModelDto> Update(UserModelDto t)
+        {
+            if (t.Name == null) return new ReturnResult<UserModelDto>() { Status = 1, Message = "用户名不能为空!" };
+            UserList.Add(t);
+            return GenerateSuccessResult(t);
+        }
+
+
+
+        [Route("api/user/deleteRang")]
+        [HttpPost]
+        public ReturnResult<UserModelDto> DeleteRang(IList<string> ids)
+        {
+            if (!ids.Any()) return new ReturnResult<UserModelDto>() { Status = 1, Message = "请选择要删除的数据!" };
+            UserList.RemoveAll(x => ids.Contains(x.Id));
+            return GenerateSuccessResult(null);
+        }
+
+        [Route("api/user/deleteCondition")]
+        [HttpPost]
+        public ReturnResult<UserModelDto> DeleteCondition(UserModelDto t)
+        {
+            if (t.Name == null) return new ReturnResult<UserModelDto>() { Status = 1, Message = "用户名不能为空!" };
+            UserList.RemoveAll(x => x.Name == t.Name);
+            return GenerateSuccessResult(t);
+        }
+
+        [Route("api/user/GetPaged")]
+        [HttpPost]
+        public PagedResult<UserModelDto> GetPaged(UserModelDto tradeSearch)
+        {
+            if (!UserList.Any())
+                InitData();
+            if (!string.IsNullOrEmpty(tradeSearch?.Name))
+            {
+                return new PagedResult<UserModelDto>() { items = UserList.Where(x => x.Name.Contains(tradeSearch.Name)).ToList(), totalCount = UserList.Count };
+            }
+            return new PagedResult<UserModelDto>() { items = UserList, totalCount = UserList.Count };
+        }
+
+        private void InitData()
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            for (int i = 0; i < 20; i++)
+            {
+                UserModelDto userModel = new UserModelDto
+                {
+                    Name = "张三" + random.Next(1000),
+                    Age = 20 + i,
+                    Address = random.Next(1000, 100000).ToString(),
+                    LoginName = "dfdf",
+                    Password = random.Next(100000, 1000000).ToString()
+                };
+                UserList.Add(userModel);
+            }
+        }
+
+
+    }
+}
